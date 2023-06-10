14b7e35c3d12a6f9f33aa4fc64ff0a06658eb6c4
TA2LSM <semihsenol@gmail.com>
2023-06-10 14:44:35 +0300

    commit

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,31 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The Job id is: {{ id }}</p>
+  <!-- getting job details from db is an async operation. So wait until job is not NULL -->
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <!-- <p>The Job id is: {{ id }}</p> -->
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
 </template>
   
 <script>
 export default {
   props: ['id'],  // accept these parameters as props from outside
-  // data() {
-  //   return {
-  //     id: this.$route.params.id
-  //   }
-  // }
+
+  data() {
+    return {
+      job: null
+    }
+  },
+
+  mounted() {
+    fetch('http://localhost:3001/jobs/' + this.id)
+      .then(res => res.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+  }
 }
 </script>
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,10 +1,15 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{name: 'JobDetails', params: { id: job.id }}">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{name: 'JobDetails', params: { id: job.id }}">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
   </div>
+<div v-else>
+  <p>Loading jobs...</p>
+</div>
 </template>
 
 <script>
@@ -15,6 +20,7 @@ export default {
       jobs: []
     }
   },
+
   mounted() {
     // String dbURL = "http://localhost:3001/jobs",
     fetch('http://localhost:3001/jobs')
